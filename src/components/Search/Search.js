67c8b83aef3866374c8ff125288ab9309ab8911b
sujeong11 <qalzm7351@gmail.com>
2022-05-19 03:30:22 +0900

    Search.js에 s-c 적용

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -2,21 +2,62 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import styled from 'styled-components';
 
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 25px 0;
+`;
+
+const DownButton = styled.button`
+  margin-right: -28px;
+  z-index: 100;
+  border: 0;
+  background-color: #ffffff;
+  
+  svg {
+    background-color: #ffffff;
+    cursor: pointer;
+  }
+`;
+
+const Input = styled.input.attrs({ 
+  type: "text",
+})`
+  width: 600px;
+  padding: 8px 30px;
+  background-color: #ffffff;
+`;
+
+const SearchButton = styled.button`
+  margin-left: -32px;
+  border: 0;
+  background-color: #ffffff;
+  
+  svg {
+    background-color: #ffffff;
+    cursor: pointer;
+  }
+`;
 
 function Search() {
   return (
-    <div>
-      <button>
+    <Container>
+      <DownButton>
         <FontAwesomeIcon icon={faCaretDown} size='lg' />
-      </button>
+      </DownButton>
       <form>
-        <input type='text' placeholder='검색어를 입력하세요' />
+        <Input 
+          placeholder='검색어를 입력하세요' 
+        />
       </form>
-      <button type='submit'>
+      <SearchButton type='submit'>
         <FontAwesomeIcon icon={faSearch} size='lg' />
-      </button>
-    </div>
+      </SearchButton>
+    </Container>
   )
 }
 
