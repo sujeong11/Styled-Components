ae96804c8a5b99270663952e40ba89e8bd7c2a77
sujeong11 <qalzm7351@gmail.com>
2022-05-19 02:18:58 +0900

    LectureList.js에 s-c 적용

--- src/components/LectureList/LectureList.js
+++ src/components/LectureList/LectureList.js
@@ -4,6 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
 import styled from 'styled-components';
 
+
 const Table = styled.table`
   margin: 0 auto;
   border-spacing: 5px;
@@ -16,20 +17,54 @@ const Caption = styled.caption`
   margin: 5px 0 8px 5px;
 `;
 
-const TableHead_TH = styled.th`
-  background-color: $dark-purple;
+const TableHeadTH = styled.th`
+  background-color: ${props => props.theme.darkPurple};
   color: #ffffff;
   height: 35px;
   width: ${props => props.width};
 `;
 
-const TableBody_TD = styled.td`
+const TableBodyTD = styled.td`
   height: 30px;
   font-size: 15px;
   background-color: #ffffff;
 `;
 
-const TableBody_TD_Professor = styled.td`
+const ProfessorTD = styled(TableBodyTD)`
+  text-align: center;
+`;
+
+const CourseTD = styled(TableBodyTD)`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 10px;
+
+  span {
+    background-color: #ffffff;
+  }
+
+  a {
+    background-color: #ffffff;
+    text-decoration: none;
+    color: ${props => props.theme.mainBlack};
+    border: 1px solid ${props => props.theme.mainBlack};
+    border-radius: 10px;
+    padding: 0 30px;
+    margin-right: 30px;
+  }
+`;
+
+const Button = styled.button`
+  border: 0;
+  background-color: #ffffff;
+  margin-left: -32px;
+
+  svg {
+    background-color: #ffffff;
+    color: ${props => props.theme.darkPurple};
+    cursor: pointer;
+  }
 `;
 
 function LectureList({ title, button }) {
@@ -49,59 +84,59 @@ function LectureList({ title, button }) {
         <Caption>{title}</Caption>
         <thead>
           <tr>
-            <TableHead_TH width="200px">교수명</TableHead_TH>
-            <TableHead_TH width="600px">과목</TableHead_TH>
+            <TableHeadTH width="200px">교수명</TableHeadTH>
+            <TableHeadTH width="600px">과목</TableHeadTH>
           </tr>
         </thead>
         <tbody>
           {/* Map으로 변경하기 */}
           <tr>
-            <td>우진운</td>
-            <td>
+            <ProfessorTD>우진운</ProfessorTD>
+            <CourseTD>
               <span>알고리즘</span>
               <Link to='#'>
                 {button}
               </Link>
-            </td>
+            </CourseTD>
             {show ?
               <td>
-                <button>
+                <Button>
                   <FontAwesomeIcon icon={faEnvelope} size='lg' />
-                </button>
+                </Button>
               </td>
               : null
             }
           </tr>
           <tr>
-            <td>우진운</td>
-            <td>
+            <ProfessorTD>우진운</ProfessorTD>
+            <CourseTD>
               <span>알고리즘</span>
               <Link to='#'>
                 {button}
               </Link>
-            </td>
+            </CourseTD>
             {show ?
               <td>
-                <button>
+                <Button>
                   <FontAwesomeIcon icon={faEnvelope} size='lg' />
-                </button>
+                </Button>
               </td>
               : null
             }
           </tr>
           <tr>
-            <td>우진운</td>
-            <td>
+            <ProfessorTD>우진운</ProfessorTD>
+            <CourseTD>
               <span>알고리즘</span>
               <Link to='#'>
                 {button}
               </Link>
-            </td>
+            </CourseTD>
             {show ?
               <td>
-                <button>
+                <Button>
                   <FontAwesomeIcon icon={faEnvelope} size='lg' />
-                </button>
+                </Button>
               </td>  
               : null
             }
