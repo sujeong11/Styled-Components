abb02f4f915bbcfc87a286ef7b6be9cfc44dae55
sujeong11 <qalzm7351@gmail.com>
2022-05-19 04:11:38 +0900

    Login.js에 s-c 적용

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,22 +1,83 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
 import Nav from '../../components/Nav/Nav';
+import styled from 'styled-components';
 
+const Container = styled.div`
+  width: 500px;
+  height: 400px;
+  margin: 110px auto;
+  border-radius: 10%;
+  box-shadow: 0 15px 15px 0 gray;
+  padding: 45px 30px;
+  background-color: #ffffff;
+
+  * {
+    background-color: #ffffff;
+  }
+`;
+
+const H2 = styled.h2`
+  text-align: center;
+  margin-bottom: 20px;
+`;
+
+const Input = styled.input`
+  width: 439px;
+  padding: 10px 10px;
+  margin-top: 20px;
+  border-radius: 8px;
+`;
+
+const LinkDiv = styled.div`
+  margin: 15px 0;
+  text-align: center;
+
+  a {
+    color: ${props => props.theme.mainBlack};
+    text-decoration: none;
+    font-size: 12px;
+
+    &:after {
+      content: '|';
+      padding: 0 10px;
+    }
+    &:last-child::after {
+      content: '';
+    }
+  }
+`;
+
+const ButtonDiv = styled.div`
+  text-align: center;
+  margin-top: 50px;
+`;
+
+const Button = styled.button`
+  width: 150px;
+  height: 40px;
+  font-size: 15px;
+  border-radius: 20px;
+  background-color: ${props => props.theme.darkPurple};
+  border: 0;
+  color: #ffffff;
+  cursor: pointer;
+`;
 
 function Login() {
   return (
-    <div>
+    <Fragment>
       <Nav />
       
-      <div>
-        <h2>로그인</h2>
+      <Container>
+        <H2>로그인</H2>
         <div>
           <form>
-            <input type='email' name='email' placeholder='이메일'></input><br />
-            <input type='password' name='password' placeholder='비밀번호'></input>
+            <Input type='email' name='email' placeholder='이메일'></Input><br />
+            <Input type='password' name='password' placeholder='비밀번호'></Input>
           </form>
         </div>
-        <div>
+        <LinkDiv>
           <Link to='#'>
             아이디 찾기
           </Link>
@@ -26,12 +87,12 @@ function Login() {
           <Link to='/join'>
             회원가입
           </Link>
-        </div>
-        <div>
-          <button type='submit'>로그인</button>
-        </div>
-      </div>
-    </div>
+        </LinkDiv>
+        <ButtonDiv>
+          <Button type='submit'>로그인</Button>
+        </ButtonDiv>
+      </Container>
+    </Fragment>
   )
 }
 
