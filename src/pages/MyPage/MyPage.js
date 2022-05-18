1992ffeaba47755e02f779bcd01e3b5f7d2c50d0
sujeong11 <qalzm7351@gmail.com>
2022-05-19 04:16:04 +0900

    MyPage.js에 s-c 적용

--- src/pages/MyPage/MyPage.js
+++ src/pages/MyPage/MyPage.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import Nav from '../../components/Nav/Nav';
 import LectureList from '../../components/LectureList/LectureList';
+import styled from 'styled-components';
 
+
+const H1 = styled.h1`
+  color: hotpink;
+  font-size: 60px;
+  width: 814px;
+  margin: 25px auto;
+`;
 
 function MyPage() {
   return (
     <div>
       <Nav />
-      <h1>MyPage</h1>
+      <H1>MyPage</H1>
       <LectureList title='나의 수강 목록' button='강의 삭제' />
     </div>
   )
